2460715472b2bc73df79dc44864a8d5727ba10bc
Matan Shenhav <matan@fluxcraft.net>
2018-03-19 20:41:05 +0100

    x

--- pkgs/development/python-modules/wordfreq/default.nix
+++ pkgs/development/python-modules/wordfreq/default.nix
@@ -5,19 +5,25 @@
 , ftfy
 , msgpack
 , pythonOlder
+, fetchPypi
 , fetchFromGitHub
 }:
 
 buildPythonPackage rec {
   pname = "wordfreq";
-  version = "1.7.0";
+  version = "2.0";
 
-  src = fetchFromGitHub {
-    owner = "ixxie";
-    repo = "wordfreq";
-    rev = "9013fa8991d98f4907d5b75280c6a23aa2742874"; 
-    sha256 = "1p31qrvr0wnrg0zdb78s59pcy0vwzlfdrk9ffcm7wb656kf6cbby";
-  };
+
+   src = fetchPypi {
+    inherit pname version;
+    sha256 = "0zbpjghv6z7xlq3445pb7fdbsz71iy4k2hv9lj67574lxmcgsabg";
+   };
+#  src = fetchFromGitHub {
+#    owner = "ixxie";
+#    repo = "wordfreq";
+#    rev = "9013fa8991d98f4907d5b75280c6a23aa2742874"; 
+#    sha256 = "1p31qrvr0wnrg0zdb78s59pcy0vwzlfdrk9ffcm7wb656kf6cbby";
+#  };
    
   propagatedBuildInputs = [ regex langcodes ftfy msgpack ];
 
